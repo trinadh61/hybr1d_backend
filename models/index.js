1b215625fe7ea3e8e3d746f4bcde404c342f589d
trinadh <trinadh>
2023-01-25 16:37:37 +0530

    adding models in node

---
+++ models/index.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize');
+const fs = require('fs');
+const path = require('path');
+const config = require('../config/config.json');
+const basename = path.basename(__filename);
+
+let db = {};
+
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+fs.readdirSync(__dirname).filter( file => {
+    return file.indexOf('.') != 0 && file != basename && file.slice(-3) == '.js'
+}).forEach((file) => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize); 
+    db[model.name] = model;
+})
+
+sequelize.sync();
+
+
+
