2d5530eea0daa23bd5473d4bb13d2f852f87d518
trinadh <trinadh>
2023-01-26 01:16:09 +0530

    added login api with token generation

--- routes/userroute.js
+++ routes/userroute.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
+const { where } = require('sequelize/lib/sequelize');
 const db = require('../models');
+const {signToken} = require('../auth')
 
 
 router.post('/signup', async (req, res, next) => {
@@ -43,4 +45,45 @@ router.post('/signup', async (req, res, next) => {
 
 })
 
+router.post('/login', async (req, res, next) => {
+    let {username, password} = req.body;
+    let error_fields = [];
+
+    if(!username)
+    error_fields.push("username");
+
+    if(!password)
+    error_fields.push("password");
+
+
+    try{
+    if(error_fields.length != 0)
+    return res.status(403).json({message : `${error_fields.join(", ")} is/are missing`})
+
+    let user = await db.user.findOne({
+        where :{
+            username : username,
+            password : password,
+        },
+        attributes : ['userid']
+    }
+    )
+
+    if(!user){
+        return res.status(404).json({message : `username/password is incorrect`})
+    }
+
+    let token = signToken(user.userid);
+    console.log(token)
+
+    return res.status(200).json({message : "user login successfully", data : {token : token}})
+
+    }
+    
+    catch(e){
+        return res.status(403).json({message : e.toString()})
+    }
+
+
+})
 module.exports = router;
