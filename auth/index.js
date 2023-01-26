0e09c3faf93c7d32f7a1e2886fb1255e560ce3d0
trinadh <trinadh>
2023-01-26 23:02:39 +0530

    added authentication on seller and buyer

--- auth/index.js
+++ auth/index.js
@@ -2,16 +2,17 @@ const jwttoken = require('jsonwebtoken')
 const privatekey = require('../config/config.json')['auth_privatekey']
 
 const authenication = (req, res, next) => {
-    const authorization_header = req.header.authorization;
+    try{
+    const authorization_header = req.headers.authorization;
 
     if(!authorization_header){
         return res.status(401).json({message : "Not Authorizied"});
     }
 
     const token = authorization_header.split(' ')[1];
-    jwttoken.verify((authorization_header, privatekey, (error, decodeToken) => {
+    jwttoken.verify(token.toString(), privatekey, (error, decodeToken) => {
         if(error){
-            return res.status(401)
+            return res.status(401).json({message : error.toString()})
         }
 
         const userid = decodeToken.userid;
@@ -19,7 +20,11 @@ const authenication = (req, res, next) => {
             return res.status(401).json({message : "User id does not match"})
         }
         next();
-    }))
+    })
+}
+catch(e){
+    return res.status(200).json({message : e.toString()})
+}
 
 }
 
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const buyer_router = require('./routes/buyerroute')
 const seller_router = require('./routes/sellerroute')
 
 const body_parser = require('body-parser');
+const { authenication } = require('./auth');
 const app = express();
 const port =  8080;
 
@@ -21,5 +22,5 @@ app.listen(port, (error) => {
 app.use(body_parser.json());
 app.use(body_parser.urlencoded({extended:true}))
 app.use('/user', user_router);
-app.use('/buyer', buyer_router);
-app.use('/seller', seller_router);
+app.use('/buyer',authenication,  buyer_router);
+app.use('/seller',authenication,  seller_router);
